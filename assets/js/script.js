f5f7687554f096d669dca451c673b64d6275b1f0
Callum Quirk <callumbquirk@gmail.com>
2023-01-31 17:40:59 +0000

    commiting logic for checking if the event is in the past, future or present with moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,45 @@
 let currentDate = $('#currentDay');
 let todayDate = moment().format("dddd, MMMM Do YYYY");
 currentDate.text(todayDate);
-console.log(moment().format("dddd, MMMM Do YYYY"))
-console.log(currentDate.text)
+
+blockTimes = [
+    $('#9AM').text(moment("09:00:00", "hour").format("h A")),
+    $('#10AM').text(moment("10:00:00", "hour").format("h A")),
+    $('#11AM').text(moment("11:00:00", "hour").format("h A")),
+    $('#12PM').text(moment("12:00:00", "hour").format("h A")),
+    $('#1PM').text(moment("13:00:00", "hour").format("h A")),
+    $('#2PM').text(moment("14:00:00", "hour").format("h A")),
+    $('#3PM').text(moment("15:00:00", "hour").format("h A")),
+    $('#4PM').text(moment("16:00:00", "hour").format("h A")),
+    $('#5PM').text(moment("17:00:00", "hour").format("h A"))
+     ]
+
+blockList = [
+    $('#9AM-textarea'),
+    $('#10AM-textarea'),
+    $('#11AM-textarea'),
+    $('#12PM-textarea'),
+    $('#1PM-textarea'),
+    $('#2PM-textarea'),
+    $('#3PM-textarea'),
+    $('#4PM-textarea'),
+    $('#5PM-textarea'),
+]
+
+for (let index = 0; index < blockTimes.length; index++) {
+    console.log(moment().isBefore(blockTimes[index]))
+    if (moment().isBefore(blockTimes[index])) {
+        blockList[index].toggleClass("future")
+    }
+    console.log(moment().isAfter(blockTimes[index]))
+    if (moment().isAfter(blockTimes[index])) {
+        blockList[index].toggleClass("past")
+    }
+    else {
+        blockList[index].toggleClass("present")
+    }
+    
+}
+
+
+
